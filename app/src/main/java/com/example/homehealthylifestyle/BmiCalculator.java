09cdd67f02e9efbee6d92e6518b3d2b89dca8b56
Isfaf-Uz-Zaman <66315494+Isfaf-Uz-Zaman@users.noreply.github.com>
2021-08-29 00:51:43 +0600

    Included the BMI calculator

--- app/src/main/java/com/example/homehealthylifestyle/BmiCalculator.java
+++ app/src/main/java/com/example/homehealthylifestyle/BmiCalculator.java
@@ -3,12 +3,48 @@ package com.example.homehealthylifestyle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class BmiCalculator extends AppCompatActivity {
+
+    private Button checkBmi;
+    private EditText userWeight;
+    private EditText userHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculator);
+
+        userWeight=findViewById(R.id.editTextTextPersonWeight);
+        userHeight=findViewById(R.id.editTextTextPersonHeight);
+        checkBmi=findViewById(R.id.checkBmiID);
+
+        checkBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateUseBMI();
+            }
+        });
+
+    }
+
+    private void calculateUseBMI() {
+
+
+        String userProvidedWeight = userWeight.getText().toString().trim();
+        String userProvidedHeight = userHeight.getText().toString().trim();
+
+        float wightValue = Float.parseFloat(userProvidedWeight);
+        float heightValue = Float.parseFloat(userProvidedHeight) / 100;
+
+        float calculatedBMI = wightValue/(heightValue * heightValue);
+
+        Toast.makeText(BmiCalculator.this,""+calculatedBMI,Toast.LENGTH_LONG).show();
+
+
     }
 }
--- app/src/main/java/com/example/homehealthylifestyle/MainActivity.java
+++ app/src/main/java/com/example/homehealthylifestyle/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this,DietChartMainActivity.class);
+                Intent intent = new Intent(MainActivity.this,BmiCalculator.class);
                 startActivity(intent);
 
             }
