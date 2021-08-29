1730c4988c1a50fc8d0821507b34712e64692860
Isfaf-Uz-Zaman <66315494+Isfaf-Uz-Zaman@users.noreply.github.com>
2021-08-30 01:11:15 +0600

    Completed the BMI calculator and rearranged the activities.

--- app/src/main/java/com/example/homehealthylifestyle/BmiCalculator.java
+++ app/src/main/java/com/example/homehealthylifestyle/BmiCalculator.java
@@ -2,10 +2,13 @@ package com.example.homehealthylifestyle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class BmiCalculator extends AppCompatActivity {
@@ -13,20 +16,46 @@ public class BmiCalculator extends AppCompatActivity {
     private Button checkBmi;
     private EditText userWeight;
     private EditText userHeight;
+    public TextView yourBmiIs,tipsForUser;
+    public Button checkTheLoseWeightPlan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculator);
 
-        userWeight=findViewById(R.id.editTextTextPersonWeight);
-        userHeight=findViewById(R.id.editTextTextPersonHeight);
-        checkBmi=findViewById(R.id.checkBmiID);
+        userWeight=(EditText)findViewById(R.id.editTextTextPersonWeight);
+        userHeight=(EditText) findViewById(R.id.editTextTextPersonHeight);
+        checkBmi=(Button) findViewById(R.id.checkBmiID);
+        yourBmiIs = (TextView) findViewById(R.id.yourBmiID);
+        tipsForUser = (TextView)findViewById(R.id.suggestUserChart);
+        checkTheLoseWeightPlan =(Button) findViewById(R.id.checkDietChart);
+
+
+        checkTheLoseWeightPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BmiCalculator.this,LoseWeightPlan.class);
+                startActivity(intent);
+            }
+        });
+
 
         checkBmi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculateUseBMI();
+                if(TextUtils.isEmpty(userWeight.getText().toString()))
+                {
+                    Toast.makeText(BmiCalculator.this,"Please provide Weight",Toast.LENGTH_LONG).show();
+                }else if(TextUtils.isEmpty(userHeight.getText().toString()))
+                {
+                    Toast.makeText(BmiCalculator.this,"Please provide Height",Toast.LENGTH_LONG).show();
+                }else {
+                    calculateUseBMI();
+                }
+
+
             }
         });
 
@@ -41,9 +70,38 @@ public class BmiCalculator extends AppCompatActivity {
         float wightValue = Float.parseFloat(userProvidedWeight);
         float heightValue = Float.parseFloat(userProvidedHeight) / 100;
 
+
         float calculatedBMI = wightValue/(heightValue * heightValue);
 
-        Toast.makeText(BmiCalculator.this,""+calculatedBMI,Toast.LENGTH_LONG).show();
+        if(calculatedBMI<16)
+        {
+            yourBmiIs.setText("Your BMI is="+calculatedBMI);
+            tipsForUser.setText("You are very much Underweight");
+
+            Toast.makeText(BmiCalculator.this," with BMI="+calculatedBMI,Toast.LENGTH_LONG).show();
+        }else if(calculatedBMI<18.4)
+        {
+            yourBmiIs.setText("Your BMI is="+calculatedBMI);
+            tipsForUser.setText("You are a bit Underweight");
+            Toast.makeText(BmiCalculator.this,"You are Underweight with BMI="+calculatedBMI,Toast.LENGTH_LONG).show();
+        }else if(calculatedBMI>=18.5 && calculatedBMI<= 24.9)
+        {
+            yourBmiIs.setText("Your BMI is="+calculatedBMI);
+            tipsForUser.setText("You are in perfect shape");
+            Toast.makeText(BmiCalculator.this,"You are in perfect weight with BMI="+calculatedBMI,Toast.LENGTH_LONG).show();
+        }else if(calculatedBMI>=25 && calculatedBMI<= 29.9)
+        {
+            yourBmiIs.setText("Your BMI is="+calculatedBMI);
+            tipsForUser.setText("You are overweight.Please check the diet plan to lose weight");
+            Toast.makeText(BmiCalculator.this,"You are in over weight with BMI="+calculatedBMI,Toast.LENGTH_LONG).show();
+        }else
+        {
+            yourBmiIs.setText("Your BMI is="+calculatedBMI);
+            tipsForUser.setText("You are in critical condition Please visit a doctor");
+            Toast.makeText(BmiCalculator.this,"You are obese with BMI="+calculatedBMI,Toast.LENGTH_LONG).show();
+        }
+
+
 
 
     }
